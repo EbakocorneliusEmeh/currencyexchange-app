2d412ca91474e83959389c3f757da6746f34037c
Ebakocornelius <ebakocornelius@gmail.com>
2025-07-21 09:38:01 +0100

    created folders

---
+++ src/components/you.jsx
@@ -0,0 +1,17 @@
+src/
+│
+├── components/
+│   ├── CurrencyCard.jsx
+│   ├── ExchangeForm.jsx
+│   ├── DepositForm.jsx
+│   ├── TotalValue.jsx
+│   └── CurrencySelector.jsx
+│
+├── context/
+│   └── WalletContext.jsx
+│
+├── utils/
+│   └── exchangeRates.js
+│
+├── App.jsx
+└── index.js
